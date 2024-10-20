89b005fce30bc71398458763bfd36312ef144778
aditya <adisingh925@gmail.com>
2024-10-20 20:06:45 +0530

    na

--- app/auth/action/page.js
+++ app/auth/action/page.js
@@ -5,7 +5,7 @@ import UpdatePassword from './form';
 export default function SignupPage() {
     return (
         <main>
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
                 <UpdatePassword />
             </Suspense>
         </main>
