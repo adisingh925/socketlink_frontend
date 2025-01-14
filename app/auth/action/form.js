0a4d5c4388b9932593db23fde405736028ea8eef
skillens.ai <skillens.ai@gmail.com>
2025-01-14 22:50:11 +0530

    na

--- app/auth/action/form.js
+++ app/auth/action/form.js
@@ -87,7 +87,7 @@ function UpdatePassword() {
         <>
             {searchParams.get("mode") === "resetPassword" && <section className="bg-blue dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-                    <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+                    <div className="w-full p-6 bg-white rounded-2xl shadow md:mt-0 sm:max-w-md dark:bg-gray-800 sm:p-8 border-2 border-white/20">
                         <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Change Password
                         </h2>
@@ -149,7 +149,7 @@ function UpdatePassword() {
             {searchParams.get("mode") === "verifyEmail" && (
                 <section className="bg-blue dark:bg-gray-900">
                     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-                        <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+                        <div className="w-full p-6 bg-white rounded-2xl shadow md:mt-0 sm:max-w-md dark:bg-gray-800 sm:p-8 border-2 border-white/20">
                             <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 {getStatusMessage()} {/* Dynamically changes */}
                             </h2>
@@ -174,7 +174,7 @@ function UpdatePassword() {
             {searchParams.get("mode") === "revertSecondFactorAddition" && (
                 <section className="bg-blue dark:bg-gray-900">
                     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-                        <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+                        <div className="w-full p-6 bg-white rounded-2xl shadow md:mt-0 sm:max-w-md dark:bg-gray-800 sm:p-8 border-2 border-white/20">
                             <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 {getStatusMessageRevert()} {/* Dynamically changes */}
                             </h2>
