bbeb64b235c871293828988695f42beeb1ab8b8b
skillens.ai <skillens.ai@gmail.com>
2025-01-17 01:42:39 +0530

    na

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -222,13 +222,13 @@ export default function FloatingNavigationBar() {
 
                     {user && (
                         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                            <button
+                            {/* <button
                                 type="button"
                                 className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                             >
                                 <span className="sr-only">View notifications</span>
                                 <BellIcon aria-hidden="true" className="h-6 w-6" />
-                            </button>
+                            </button> */}
                             <Menu as="div" className="relative ml-3">
                                 <div>
                                     <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
