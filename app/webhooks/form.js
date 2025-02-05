7afdf2e9c0ca9f84327558ea5d0aed5b3d922e59
skillens.ai <skillens.ai@gmail.com>
2025-02-05 22:47:25 +0530

    na

--- app/webhooks/form.js
+++ app/webhooks/form.js
@@ -380,7 +380,7 @@ function WebhookManagement() {
 
     return (
         <>
-            <div className="flex flex-col h-[100dvh] dark:bg-gray-900">
+            <div className="flex flex-col dark:bg-gray-900">
                 <FloatingNavigationBar />
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6 py-10 mt-20 dark:bg-gray-900">
                     <div className="w-full max-w-lg p-4 sm:p-8 bg-gray-800 text-white rounded-2xl shadow-xl border-2 border-white/20 overflow-hidden self-start flex flex-col">
