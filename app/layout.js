0ea16926aef3bf8e2360f3ad965d85e9a11c987c
skillens.ai <skillens.ai@gmail.com>
2025-04-10 20:13:53 +0530

    na

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 // Import Roboto using next/font
 const roboto = Roboto({
@@ -18,6 +19,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Ads Tag */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16656132114"
+        />
+        <Script
+          id="google-ads"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16656132114');
+          `}
+        </Script>
+      </head>
       <body className={`${roboto.variable} antialiased`}>
         {children}
       </body>
